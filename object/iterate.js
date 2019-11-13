61a1405c5ae491c137b537870357adf9c8fe933c
rotcivDev <vsallesa@gmail.com>
2019-11-13 20:57:25 -0300

    iterate update

--- object/iterate.js
+++ object/iterate.js
@@ -1,8 +1,12 @@
 //iterating over an object
-
 var username = {
   first: 'John',
   last: 'Doe',
+  age: 80,
+  adress: {
+    street: 'Deathroad Street',
+    city: 'Doomsday'
+  }
 };
 
 for (let u in username) {
@@ -18,14 +22,28 @@ for (let u of Object.keys(username)) {
 const entriesIterate = Object.entries(username);
 console.log(entriesIterate);
 
-entriesIterate.forEach(([key, value]) => {
-  console.log(key, value);
-  });
+const obj = {};
+Object.assign(obj, entriesIterate);
+console.log(obj);
 
+entriesIterate.forEach(arr => {
+console.log(arr.toString().split(',').join(' '));
+});
 
-let newUsername = entriesIterate.reduce((o, key) => ({
-  ...o, key, [key[0]]: o[1],
-})); 
-  
+function arrayIndexCounter(arr) {
+  return arr.length;
+}
+console.log(arrayIndexCounter(entriesIterate));
 
-console.log(newUsername);
+function indexFilter(icounter) {
+  return icounter % 2 === 0 ? 'par' : 'impar'; 
+}
+console.log(indexFilter(arrayIndexCounter(entriesIterate)))
+
+const indexRemover = arr => {
+  let lastIndex = arr.length -= 1;
+  arr.splice(lastIndex, 1, 'oops');
+  return arr
+};
+console.log(indexRemover(entriesIterate))
+console.log(entriesIterate)
